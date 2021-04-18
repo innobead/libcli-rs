029d0ded19f87e892730e4b12bf87b4611ae7f8c
David Ko <dko@suse.com>
2021-04-19 01:35:39 +0800

    Update to 0.1.2

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,11 +1,13 @@
 use std::io::Write;
 use std::str::FromStr;
 
+use crate::output::console::ConsoleOutput;
+use crate::output::json::JsonOutput;
+use crate::output::yaml::YamlOutput;
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 pub mod console;
-pub mod factory;
 pub mod json;
 pub mod yaml;
 
@@ -35,6 +37,40 @@ impl FromStr for OutputFormat {
             "json" => Ok(OutputFormat::Json),
             "yaml" => Ok(OutputFormat::Yaml),
             _ => Err(format!("{} not supported format", s)),
+        }
+    }
+}
+
+pub struct OutputFactory {
+    pub output: OutputFormat,
+}
+
+impl OutputFactory {
+    pub fn new(output: OutputFormat) -> Self {
+        Self { output }
+    }
+}
+
+impl OutputTrait for OutputFactory {
+    fn display<'a, T: Deserialize<'a> + Serialize>(
+        &self,
+        writer: impl Write,
+        obj: &T,
+        include_keys: Option<Vec<&str>>,
+        exclude_keys: Option<Vec<&str>>,
+    ) -> Result<()> {
+        match self.output {
+            OutputFormat::Console => {
+                ConsoleOutput::new().display(writer, obj, include_keys, exclude_keys)
+            }
+
+            OutputFormat::Yaml => {
+                YamlOutput::new().display(writer, obj, include_keys, exclude_keys)
+            }
+
+            OutputFormat::Json => {
+                JsonOutput::new().display(writer, obj, include_keys, exclude_keys)
+            }
         }
     }
 }
